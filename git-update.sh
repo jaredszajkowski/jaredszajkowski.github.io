9929b0b1096d8f135321951e792716b1d7988612
Jared Szajkowski <jared.szajkowski@gmail.com>
2025-02-03 20:03:10 -0600

    Regen site

--- git-update.sh
+++ git-update.sh
@@ -3,4 +3,5 @@ echo What is the commit message?
 
 read message
 
+# rm -r public/* && hugo && git add . && git commit -am "$message" && git push
 hugo && git add . && git commit -am "$message" && git push
