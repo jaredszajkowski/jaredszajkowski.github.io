2c57f3d012d80e029c210428f3cac76982a961f0
Jared Szajkowski <jared.szajkowski@gmail.com>
2025-03-21 15:05:11 -0500

    Updating post files

--- public/2025/03/01/investigating-a-vix-trading-signal/convert_notebook.py
+++ public/2025/03/01/investigating-a-vix-trading-signal/convert_notebook.py
@@ -1,3 +1,4 @@
+# Simple functions to convert a juptyer notebook to a various formats
 import subprocess
 import shutil
 from pathlib import Path
@@ -10,39 +11,23 @@ args = parser.parse_args()
 
 notebook = args.notebook
 
-# Paths
 script_directory = Path(__file__).parent
-project_root = script_directory.parents[2]  # Adjust if necessary
-static_directory = project_root / "static"
-
 print("Script Directory:", script_directory)
-print("Project Root:", project_root)
-print("Static Directory:", static_directory)
 
+# Function to convert Jupyter notebook to various formats
 def convert_notebook(notebook: str, to_format: str):
-    notebook_path = script_directory / f"{notebook}.ipynb"
-    output_path = script_directory / f"{notebook}.{to_format}"
-
+    output_file = script_directory / f"{notebook}.{to_format}"
     cmd = [
         "jupyter", "nbconvert",
         f"--to={to_format}",
         "--log-level=WARN",
-        f"--output={output_path.name}",
-        str(notebook_path)
+        f"--output={output_file}",
+        f"{script_directory}/{notebook}.ipynb"
     ]
 
     try:
         subprocess.run(cmd, check=True)
         print(f"✅ Converted {notebook}.ipynb to {to_format}.")
-
-        # Copy HTML to static/{notebook}/
-        if to_format == "html":
-            static_target_dir = static_directory / notebook
-            static_target_dir.mkdir(parents=True, exist_ok=True)
-
-            destination = static_target_dir / output_path.name
-            shutil.copy2(output_path, destination)
-            print(f"📁 Copied {output_path.name} to {destination}")
     except subprocess.CalledProcessError as e:
         print(f"❌ Error converting {notebook}.ipynb to {to_format}: {e}")
 
