28d4951b00c3299d0d7ac6631d9da11ec3d4f3a7
Jared Szajkowski <jared.szajkowski@gmail.com>
2025-03-22 16:18:56 -0500

    Pushing built hugo site

--- git-update.sh
+++ git-update.sh
@@ -3,5 +3,4 @@ echo What is the commit message?
 
 read message
 
-rm -r public/* && hugo && git add . && git commit -am "$message" && git push
-# hugo && git add . && git commit -am "$message" && git push
+git add . && git commit -am "$message" && git push
